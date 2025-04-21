46b251102731c1405c8c3ff16ed4949b3bdafb09
unknown <abdtsegaye@gmail.com>
2025-04-21 16:13:11 +0300

    added umami tracking code, fixed process.env issues

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
       <link rel="icon" href="" />
+      <script defer src="http://138.197.16.211:3030/script.js" data-website-id="46377265-1160-4312-95bc-a12dc9495347"></script>
     </>
   );
 }
