c4607bd814fb5a7b19da4fdc2f4c5edbd370827d
Abdisa Tsegaye <48585612+abdisaongithub@users.noreply.github.com>
2025-08-10 13:42:08 +0300

    added hiding landing page logic

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -10,33 +10,73 @@ import "../globals.css";
 import ToasterContext from "../context/ToastContext";
 
 export default function RootLayout({
-                                     children,
-                                   }: {
+  children,
+}: {
   children: React.ReactNode;
 }) {
+  const [landingHidden, setLandingHidden] = React.useState(true);
+
+  const showPrompt = () => {
+    let inputCode = prompt("Please enter your code:", "");
+    const code = '1234abcd'
+    if (inputCode != null) {
+      if (inputCode === code) {
+        setLandingHidden(false);
+      }
+    }
+  }
+
   return (
     <html lang="en" suppressHydrationWarning>
-    <head>
-      {/* Boldonse */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link href="https://fonts.googleapis.com/css2?family=Boldonse&display=swap" rel="stylesheet" />
-      {/* Roboto with all weights */}
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-        rel="stylesheet"
-      />
-    </head>
-    <body className="font-roboto bg-white dark:bg-[#151A21]">
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <Lines />
-      <Header />
-      <ToasterContext />
-      {children}
-      <Footer />
-      <ScrollToTop />
-    </ThemeProvider>
-    </body>
+      <head>
+        {/* Boldonse */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Boldonse&display=swap"
+          rel="stylesheet"
+        />
+        {/* Roboto with all weights */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body
+        className={`boldonse font-roboto bg-white  ${
+          landingHidden ? "" : "dark:bg-[#151A21]"
+        }`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {landingHidden ? (
+            <div className={`flex h-screen items-center justify-center`}>
+              <div className="flex-col justify-items-center">
+                <img
+                  src={`/images/construction.svg`}
+                  className={`h-66 mx-auto`}
+                  alt={`construction image`}
+                  onDoubleClick={() => showPrompt()}
+                />
+                <p className={`mx-auto mt-2 text-5xl text-black`}>Coming Soon</p>
+                <p className={`mx-auto text-xl`}>Site Under Construction</p>
+              </div>
+            </div>
+          ) : (
+            <>
+              <Lines />
+              <Header />
+              <ToasterContext />
+              {children}
+              <Footer />
+              <ScrollToTop />
+            </>
+          )}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
