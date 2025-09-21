2f62fa88808319a7bf0ad01db74374934415e019
unknown <abdtsegaye@gmail.com>
2025-09-22 02:10:21 +0300

    fixed taxLottie.tsx issues

--- app/(site)/services/taxLottie.tsx
+++ app/(site)/services/taxLottie.tsx
@@ -1,14 +1,23 @@
 "use client";
-import React from "react";
-import Lottie from "lottie-react";
+import React, { useRef } from "react";
 
 const LottieAnimation = () => {
+  const ref = useRef(null);
+  React.useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
+
   return (
     <div className="w-full h-[500px] transform -translate-y-12">
-      <Lottie
-        animationData={require("/public/images/shape/tax-01.json")}
+      <lottie-player
+        id="firstLottie"
+        ref={ref}
+        autoplay
         loop
-      />
+        mode="normal"
+        src="/images/shape/tax-01.json"
+        style={{  width: "400px", height: "400px" }}
+      ></lottie-player>
     </div>
   );
 };
